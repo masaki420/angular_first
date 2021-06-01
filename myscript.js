5ce9b8babc773e79d7a33d145228971a0c154df3
masaki420 <zyab5275@gmail.com>
2021-06-02 07:46:44 +0900

    扶養ファイルの削除

--- myscript.js
+++
@@ -1,8 +0,0 @@
-angular.module('myapp', [])
-    .controller('MainController', ['$scope', function($scope) {
-        $scope.users = [
-            {"name":"taguchi", "score":52.22},
-            {"name":"tanaka", "score":38.22},
-            {"name":"yamada", "score":11.11}
-        ];
-    }]);
