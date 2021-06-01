0ee3eea2a6769d9e5f5a53883e606f6978992072
masaki420 <zyab5275@gmail.com>
2021-06-02 03:53:24 +0900

    情報がとても古いらしい

--- myscript.js
+++ myscript.js
@@ -1,12 +1,3 @@
-// angular.module('my app',[])
-// 	.controller('MainController',['$scope', function($scope){
-// 		$scope.users = [
-// 			{"name":"aihara","score":52.22},
-// 			{"name":"aikawa","score":38.22},
-// 			{"name":"aiue","score":11.11}
-// 		];
-// 	}]);
-
 angular.module('myapp', [])
     .controller('MainController', ['$scope', function($scope) {
         $scope.users = [
